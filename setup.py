c34f833471be8287f416fa2d85429a7d42bc7b2b
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2018-10-17 19:35:44 -0400

    CI: Drop untestable python 3.7

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Tcl',
         'Topic :: Software Development :: Code Generators',
         'Topic :: System :: Archiving :: Packaging',
